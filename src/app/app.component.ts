e4c91ea48b80bbc23211ea9c2c2922e2b7d2382d
zdravkokirilov <zdravkovasilchevkirilov@gmail.com>
2021-07-05 17:42:45 +0300

    media queries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: { class: 'full-container' },
 })
 export class AppComponent {
   title = 'goatzilla';
