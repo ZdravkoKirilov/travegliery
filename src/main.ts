241147f8f776ae5446c3ba879b25a17a2aade9d0
zdravkokirilov <zdravkovasilchevkirilov@gmail.com>
2021-07-17 12:34:48 +0300

    todos

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,16 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  /* TODO
+
+participant notes
+participant message history
+participant groups besides "Favorites"?
+send email to group?
+
+booking -> send reminder
+
+
+  */
